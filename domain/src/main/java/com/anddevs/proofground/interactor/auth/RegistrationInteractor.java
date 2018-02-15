318a3ec6dff08af08a9b4f4003106bec63ea50df
homepunk/work <nikita.storozhenko@gmail.com>
2018-02-15 17:35:53 +0200

    domain module setup

---
+++ domain/src/main/java/com/anddevs/proofground/interactor/auth/RegistrationInteractor.java
@@ -0,0 +1,8 @@
+package com.anddevs.proofground.interactor.auth;
+
+/**
+ * Created by Homepunk on 15.02.2018.
+ **/
+
+public class RegistrationInteractor {
+}
---
+++ domain/src/main/java/com/anddevs/proofground/validator/AuthValidator.java
@@ -0,0 +1,8 @@
+package com.anddevs.proofground.validator;
+
+/**
+ * Created by Homepunk on 15.02.2018.
+ **/
+
+public class AuthValidator {
+}
