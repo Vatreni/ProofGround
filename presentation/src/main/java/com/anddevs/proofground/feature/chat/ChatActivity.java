1143c35ea87c21c37adbef18672d3879e06e9a22
Taras <gorthaur12@gmail.com>
2018-02-20 00:28:20 +0200

    added ui

--- presentation/src/main/java/com/anddevs/proofground/feature/chat/ChatActivity.java
+++ presentation/src/main/java/com/anddevs/proofground/feature/chat/ChatActivity.java
@@ -1,8 +1,21 @@
 package com.anddevs.proofground.feature.chat;
 
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anddevs.proofground.R;
+
 /**
  * Created by Homepunk on 15.02.2018.
  **/
 
-public class ChatActivity {
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_chat_layout);
+    }
 }
